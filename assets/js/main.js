8ca8a842da7609c2a11bca1ce4102a6baecf3e92
kayky_paulino <98046923+kaykypaulinoalvesdossantos@users.noreply.github.com>
2022-03-31 16:30:01 -0300

    About

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,13 +37,6 @@ function scrollHeader(){
 
 window.addEventListener('scroll' , scrollHeader)
 
-/*function scrollHeader(){
-    const header = document.getElementById('header')
-    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
-}
-window.addEventListener('scroll', scrollHeader)
-*/
 /*=============== POPULAR SWIPER ===============*/
 
 
