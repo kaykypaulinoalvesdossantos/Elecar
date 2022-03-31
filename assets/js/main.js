ff1aa255f7e6a28cb74978fc6083206a60e89cfb
kayky_paulino <98046923+kaykypaulinoalvesdossantos@users.noreply.github.com>
2022-03-31 16:15:41 -0300

    Home

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,8 +30,20 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
 
+window.addEventListener('scroll' , scrollHeader)
 
+/*function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+*/
 /*=============== POPULAR SWIPER ===============*/
 
 
