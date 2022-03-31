f5f9279c104c47cfe5db32f4d15308c20c673929
kayky_paulino <98046923+kaykypaulinoalvesdossantos@users.noreply.github.com>
2022-03-31 14:37:10 -0300

    Estrutura | Header | Nav menu
    Estrutura de arquivos
    Heder da pagina
    Menu de navegação mobile

---
+++ assets/js/main.js
@@ -0,0 +1,50 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+
+/* ===== MENU SHOW =====*/
+/* Validate if constant exist */
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/* ===== MENU HIDDEN =====*/
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== POPULAR SWIPER ===============*/
+
+
+/*=============== MIXITUP FILTER FEATURED ===============*/
+
+
+/* Link active featured */ 
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
